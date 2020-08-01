4cc83b6fc89429e3d681b172e320822d89f2967a
Tomasz Michnik <tom.michnik@gmail.com>
2020-08-01 06:03:18 +0200

    Updated notes and code

--- README.md
+++ README.md
@@ -3,4 +3,28 @@
 #### Intro to Microservices
 
 - [12factor.net](https://12factor.net/)
-
+- What are microservices:
+  - Microservices small targeted services 
+  - Each service has its own repository 
+  - Isolated from other services 
+  - Should not be bundled with other services when deployed 
+  - Loosely coupled 
+  - When interacting with other services, should be done in a technology agnostic manner ie  Restful web services - HTTP / JSON
+- With a Microservice Architecture:
+  - Applications are composed using individual microservices 
+  - Each service will typically have its own database 
+  - Each microservice is independently deployable
+  - Scaling of individual services is now possible 
+  - CI/CD becomes easier since services are smaller and less complex to deploy
+- Pros:
+  - Easy to understand & develop - Services are smaller and more targeted 
+  - Software Quality - Since services are more targeted and have a limited scope 
+  - Scalability - Independent services can be scaled up and down to the application’s demands. 
+  - Reliability - Software bugs are isolated 
+  - Technology flexibility - Services can be developed using any language or technology stack. 
+- Cons:
+  - Integration testing can be difficult
+  - Deployments are more complex. Rather than one application to deploy, you now have many. 
+  - Operational cost with each service - Each service is a small application 
+  - Needs own repo, own deployment process, own database, etc 
+  - Additional hardware resources - Additional services need additional hardware to run on
--- mssc-brewery-client-master/src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
+++ mssc-brewery-client-master/src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -5,24 +5,38 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
 
+    private final Integer maxTotalConnections;
+    private final Integer defaultMaxTotalConnections;
+    private final Integer connectionRequestTimeout;
+    private final Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizer(@Value("${maxtotalconnections}") Integer maxTotalConnections, @Value("${defaultmaxtotalconnections}")  Integer defaultMaxTotalConnections, @Value("${connectionrequesttimeout}")  Integer connectionRequestTimeout, @Value("${sockettimeout}")  Integer socketTimeout) {
+        this.maxTotalConnections = maxTotalConnections;
+        this.defaultMaxTotalConnections = defaultMaxTotalConnections;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
+
     public ClientHttpRequestFactory clientHttpRequestFactory(){
         PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
-        connectionManager.setMaxTotal(100);
-        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setMaxTotal(maxTotalConnections);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnections);
 
         RequestConfig requestConfig = RequestConfig
                 .custom()
-                .setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients

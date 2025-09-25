3b9a47612c29b9b7a54b00c8753958c4ca303e31
tommic81 <tom.michnik@gmail.com>
2025-09-25 21:55:39 +0200

    RequestBody fix

--- mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -29,7 +29,7 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity handlePost(BeerDto beerDto) {
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
 
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
         HttpHeaders headers = new HttpHeaders();
@@ -39,7 +39,7 @@ public class BeerController {
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
 
         beerService.updateBeer(beerId, beerDto);
 
